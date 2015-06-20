2a162033c55efaa86147aa0f2267233c01aba893
KyleGW <kyle.gatewood+GitHub@gmail.com>
2015-06-20 15:09:03 -0500

    added Write-Log snippet

---
+++ Write-Log.ps1
@@ -0,0 +1,24 @@
+﻿$global:logFilePath = "d:\logs"
+$global:logFileName = "$((Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))-LogfileName.log"
+
+Function Write-Log
+{
+    Param ([string]$textToWriteToLog)
+
+    $LogFile = Join-Path $logFilePath $logFileName
+    
+    if (!(Test-Path -path $logFile ))
+    {
+        $logFile = New-Item -type file $logFile
+    }
+    Write-Verbose $textToWriteToLog
+    Try
+    {
+        Add-content $logfile -value "$((Get-Date).ToString("yyyy-MM-dd HH:mm:ss.fff")) - $textToWriteToLog"
+    }
+    catch [system.UnauthorizedAccessException]
+    {
+        throw "Error (logfile): Write access denied to [$logFile]" 
+    }
+}
+
