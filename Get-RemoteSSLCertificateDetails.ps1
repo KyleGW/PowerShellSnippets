8ac91767bea12e9df9cc10f218d23358c9727dfc
KyleGW <kyle.gatewood+GitHub@gmail.com>
2015-06-20 16:33:49 -0500

    Added script to get details of a remote SSL certificate via URL

---
+++ Get-RemoteSSLCertificateDetails.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-RemoteSSLCertificateDetails
+{
+    param($url)
+
+    Write-Log "Attempting connection to $url"
+    $request = $null
+    $request = [Net.HttpWebRequest]::Create($url)
+    $request.Timeout = 1200
+
+    try 
+    {
+        $request.GetResponse() | Out-Null
+    }
+    catch
+    {
+        Write-Log "Error connecting to URL $url`: $_"
+    }
+    $cert = $null
+    $cert = $request.ServicePoint.Certificate
+
+    if($cert)
+    {
+        $hash = @{
+            result = "Success"
+            ExpirationDate = [datetime]$cert.GetExpirationDateString()
+            ExpiresIn = $(([DateTime]$cert.GetExpirationDateString() - $(Get-Date)).Days)
+            Name = $cert.GetName()
+            PublicKeyString = $cert.GetPublicKeyString()
+            SerialNumber = $cert.GetSerialNumberString()
+            Thumbprint = $cert.GetCertHashString()
+            EffectiveDate = $cert.GetEffectiveDateString()
+            Issuer = $cert.GetIssuerName()
+        }
+
+        #create a new powershell object out of the data so that we get all of the PS Object goodness
+        $Certificate = New-Object PSObject -Property $hash
+
+        return $Certificate
+    }
+    else
+    {
+        $return =  New-Object PSObject
+        Add-Member -InputObject $return -MemberType NoteProperty -Name result -Value "fail"
+        return $return
+    }
+}
